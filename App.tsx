89b8b8df6b23edad0fe1e3fd49af3149d2181f23
Decimirr <kim0724@snu.ac.kr>
2023-03-14 12:58:36 +0900

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
+
+import 'expo-dev-client';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      originWhitelist={['*']}
+      source={{ html: '<h1><center>Hello world</center></h1>' }}
+    />
   );
 }
 
